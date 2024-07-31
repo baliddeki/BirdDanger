594e46bf6096321d85f24bc8464ce99278b17075
baliddeki <baliddeki@gmail.com>
2024-08-01 00:25:46 +0300

    upload empty repo

--- BirdDanger/settings.py
+++ BirdDanger/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', "nursing-emlynn-birddanger-af37826c.koyeb.app, birddanger.nursing-emlynn.internal:8000").split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', "localhost, 127.0.0.1:8000, nursing-emlynn-birddanger-af37826c.koyeb.app, birddanger.nursing-emlynn.internal:8000").split(',')
 
 # Application definition
 
