ffe22e836577ee1ae196723518a74590a2ca2f1b
baliddeki <baliddeki@gmail.com>
2024-07-31 16:14:25 +0300

    change to settings file for bad request fix

--- BirdDanger/settings.py
+++ BirdDanger/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'nursing-emlynn-birddanger-af37826c.koyeb.app').split(',')
 
 # Application definition
 
